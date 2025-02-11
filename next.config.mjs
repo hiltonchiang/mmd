2d86a4ea35d994a80f6316052e5fa1946e42a36a
hiltonchang <hiltonchiang@gmail.com>
2025-02-12 07:15:39 +0800

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
   output,
   basePath,
   productionBrowserSourceMaps: true,
+  async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/(.*)",
+                headers: [
+                    // other headers omitted for brevity...
+                    { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
+                    { key: "Cross-Origin-Embedder-Policy", value: "require-corp" }
+                ]
+            }
+        ]
     }
 }
  
