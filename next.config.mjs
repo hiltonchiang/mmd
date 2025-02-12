4cb6f47fbbac82cd7a174b558576285b55a60053
hiltonchang <hiltonchiang@gmail.com>
2025-02-12 08:19:51 +0800

    assetPrefix added on next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const output = process.env.EXPORT ? 'export' : undefined
 const basePath = process.env.BASE_PATH || undefined
-
+const assetPrefix = process.env.ASSET_PREFIX ? process.env.ASSET_PREFIX.concat('/') : undefined
 const nextConfig = {
   output,
   basePath,
+  assetPrefix,
   productionBrowserSourceMaps: true,
   async headers() {
         return [
